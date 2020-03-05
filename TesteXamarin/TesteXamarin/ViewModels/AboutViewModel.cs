1a00ee84079d1f8a036c8b1115c76816638956cc
ronilson.silva <ronilson.silva@toctec.eng.br>
2020-03-05 17:09:34 -0300

    Genêsis

---
+++ TesteXamarin/TesteXamarin/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TesteXamarin.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "Sobre";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/ronilsonsilva/overview_xamarin"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
